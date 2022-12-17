9f9dfe915636e25ee8e925f5f6b570bf6b83ac49
Emmanuel <emmapraise@gmail.com>
2022-12-17 09:11:51 +0100

    Feat: db connection

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': 
+    # [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50
+}
